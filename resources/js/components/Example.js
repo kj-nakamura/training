15a042fce0458ab0740f2ac7509a771590c08bd7
n.kenji <n.kenji@dayneeds.jp>
2020-02-20 18:25:49 +0900

    カテゴリごとにID表示

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -16,26 +16,44 @@ function RenderRows(props){
     });
 }
 
+function TrainingCategory(props) {
+    return props.categories.map(category => {
+        return (
+            <option key={category.id} value={category.id}>{category.name}</option>
+        );
+    });
+}
+
 export default class Example extends Component {
 
     constructor() {
         super();
         this.state = {
             posts: [],
-            post: ''
+            post: '',
+            categories: [],
+            category: '',
         };
         this.inputChange = this.inputChange.bind(this);
         this.addPost = this.addPost.bind(this);
         this.deleteTask = this.deleteTask.bind(this);
+        this.changeCategory = this.changeCategory.bind(this);
     }
 
     componentDidMount() {
-        axios.get('/api/posts')
-            .then(response => {
-                this.setState({posts: response.data});
+        function getPosts() {
+            return axios.get('/api/posts');
+        }
+        function getCategories() {
+            return axios.get('/api/categories');
+        }
+        Promise.all([getPosts(), getCategories()])
+            .then(([response1, response2]) => {
+                this.setState({posts: response1.data});
+                this.setState({categories: response2.data});
             })
             .catch(() => {
-                console.log(error);
+                console.log('未取得');
             });
     }
 
@@ -91,9 +109,31 @@ export default class Example extends Component {
             });
     }
 
+    //カテゴリが変更されたら（都度）
+    changeCategory(event){
+        switch(event.target.name){
+            case 'category':
+                this.setState({
+                    category: event.target.value
+                });
+                break;
+            default:
+                break;
+        }
+    }
+
     render() {
         return (
             <div className="container">
+                {/*カテゴリ*/}
+                <select className="form-control col-md-4" name="category" onChange={this.changeCategory}>
+                    <TrainingCategory categories={this.state.categories}/>
+                </select>
+                {/* イベント */}
+                <select>
+                    <option value={this.state.category}>{this.state.category}</option>
+                </select>
+
                 {/* add from */}
                 <div className="form-group mt-4">
                     <label htmlFor="post">新規Post</label>
