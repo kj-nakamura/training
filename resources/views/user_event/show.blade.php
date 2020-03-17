1a7720c944276777b87032650b4340115ab4a59a
n.kenji <n.kenji@dayneeds.jp>
2020-03-17 12:44:17 +0900

    ビュー編集

--- resources/views/user_event/show.blade.php
+++ resources/views/user_event/show.blade.php
@@ -6,15 +6,18 @@
         <div class="col-md-8">
             <div class="card">
             <div class="card-header">{{ $user_event->name }}　<span>{{ $user_event->event_at }}</span></div>
-
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                  <form role="form" class="form" method="POST" action="{{ route('web::weight_count.add', $user_event) }}">
+                    {{ csrf_field() }}
+                  <input type="number" name="weight" min="1" value="{{ $user_event->weight_counts->max('weight')}}" required>kg
+                  <input type="number" name="count" min="1" value="10" required>回
+                    <button type="submit" class="btn btn-primary text-right">Add</button>
+                  </form>
+                  <ul>
+                    @foreach($user_event->weight_counts as $weight_count)
+                  <li>{{ $loop->index + 1 }}：{{ $weight_count->weight   }}kg ☓ {{ $weight_count->count }}回</li>
+                    @endforeach
+                  </ul>
                 </div>
             </div>
         </div>
